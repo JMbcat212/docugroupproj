bd9432736f01ac6c59098389c07e66c02ab7f417
prachetas98 <pd040615@ohio.edu>
2018-10-01 18:19:21 -0400

    Added comments to piece.h

--- piece.h
+++ piece.h
@@ -10,7 +10,9 @@ enum color {black, white, blank};
 class piece {
 public:
 	piece() {theColor = blank;}
-
+	
+	/*function: This function flips the colour of the piece while playing the game 
+	*/
 	void flip()
 	{
 		if (theColor == white) {
@@ -21,11 +23,11 @@ public:
 		}
 	}
 
-	bool is_blank()const {return theColor == blank;}
-	bool is_black()const {return theColor == black;}
-	bool is_white()const {return theColor == white;}
-	void set_white() {theColor = white;}
-	void set_black() {theColor = black;}
+	bool is_blank()const {return theColor == blank;} /// this function returns a blank colour
+	bool is_black()const {return theColor == black;} /// this function returns a black colour
+	bool is_white()const {return theColor == white;} /// this function returns a white colour
+	void set_white() {theColor = white;} /// this function sets the color of piece to white
+	void set_black() {theColor = black;} /// This function sets the color of piece to black
 
 private:
 	color theColor;
